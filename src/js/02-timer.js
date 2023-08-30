9b454df4d2e5759fc1e97ac4d5c1fa14609f570a
Vlad <vadya-85@mail.ru>
2023-08-31 00:53:54 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -19,10 +19,9 @@ const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
-  mode: 'single',
   minuteIncrement: 1,
   locale: Ukrainian,
-  onOpen() {},
+  targetTime: 0,
   onChange() {
     clearInterval(intervalId);
     refs.days.textContent = addLeadingZero(0);
@@ -30,7 +29,7 @@ const options = {
     refs.minutes.textContent = addLeadingZero(0);
     refs.seconds.textContent = addLeadingZero(0);
   },
-  onClose(selectedDates) {
+  onClose(selectedDates, dateStr, instance) {
     if (selectedDates[0] - new Date() <= 0) {
       refs.startBtn.setAttribute('disabled', true);
       Notify.failure('Please choose a date in the future', {
@@ -38,34 +37,31 @@ const options = {
       });
       return;
     }
-    const targetTime = selectedDates[0];
+    instance.config.targetTime = selectedDates[0];
     refs.startBtn.removeAttribute('disabled');
-    refs.startBtn.addEventListener('click', onStartBtnClick);
-
-    function onStartBtnClick() {
-      refs.startBtn.setAttribute('disabled', true);
-      refs.startBtn.removeEventListener('click', onStartBtnClick);
-
-      intervalId = setInterval(() => {
-        const currentTime = new Date();
-        const timeleft = targetTime - currentTime;
-        const convertedTime = convertMs(timeleft);
-        refs.days.textContent = addLeadingZero(convertedTime.days);
-        refs.hours.textContent = addLeadingZero(convertedTime.hours);
-        refs.minutes.textContent = addLeadingZero(convertedTime.minutes);
-        refs.seconds.textContent = addLeadingZero(convertedTime.seconds);
-        if (Object.values(convertedTime).every(element => element === 0)) {
-          clearInterval(intervalId);
-          refs.startBtn.setAttribute('disabled', true);
-        }
-      }, 1000);
-      
-    }
-    
   },
 };
 
-flatpickr(refs.input, options);
+let fp = flatpickr(refs.input, options);
+
+refs.startBtn.addEventListener('click', onStartBtnClick);
+
+function onStartBtnClick() {
+  refs.startBtn.setAttribute('disabled', true);
+  intervalId = setInterval(() => {
+    const currentTime = new Date();
+    const timeleft = fp.config.targetTime - currentTime;
+    const convertedTime = convertMs(timeleft);
+    refs.days.textContent = addLeadingZero(convertedTime.days);
+    refs.hours.textContent = addLeadingZero(convertedTime.hours);
+    refs.minutes.textContent = addLeadingZero(convertedTime.minutes);
+    refs.seconds.textContent = addLeadingZero(convertedTime.seconds);
+    if (Object.values(convertedTime).every(element => element === 0)) {
+      clearInterval(intervalId);
+      // refs.startBtn.setAttribute('disabled', true);
+    }
+  }, 1000);
+}
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
