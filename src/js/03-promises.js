4c8861f548ed9db3885c79e4614deb90107dc506
Vlad <vadya-85@mail.ru>
2023-08-31 00:59:42 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,22 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`, {
+  clickToClose: true,
+});
+
+Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`, {
+  clickToClose: true,
+});
+
+
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
